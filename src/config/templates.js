067a649b6f50da806eae9280cf9e3bb477686fb4
Xiaolong96 <xiaolongupc@163.com>
2020-07-29 02:39:11 +0800

    修改模版配置

--- src/config/templates.js
+++ src/config/templates.js
@@ -1,23 +1,8 @@
 const templates = [
   {
-    name: "rephic",
-    repo: "https://github.com/pspgbhu/Rephic.git",
-    desc: "React 同构直出脚手架"
-  },
-  {
-    name: "koa-ts-template",
-    repo: "https://github.com/jd-smart-fe/koa-ts-template.git",
-    desc: "基于 Koa 的 TypeScript 工程脚手架"
-  },
-  {
-    name: "hammer-site",
-    repo: "https://github.com/xieyankun/HammerSite.git",
-    desc: "H5 静态页面工程脚手架"
-  },
-  {
-    name: "react-template",
-    repo: "https://github.com/simqifeng/react-template.git",
-    desc: "React 工程脚手架"
+    name: "react-ts",
+    repo: "https://github.com/Xiaolong96/react-ts-template.git",
+    desc: "React TypeScript 工程脚手架"
   }
 ];
 
